7c8202be93de5f4a10c590e7e38d88d7e3052a2c
RashikZ <rashique3457@gmail.com>
2024-04-03 23:36:44 -0400

    Updated Design

--- TTC-App/src/App.jsx
+++ TTC-App/src/App.jsx
@@ -1,27 +1,30 @@
 import { Menu } from "./components/Menu";
-import { get_routes } from "./api/routes"
-import { useEffect } from "react";
-import { useState } from "react";
-import './App.css'
+import { get_routes } from "./api/routes";
+import { useEffect, useState } from "react";
+import logo from "./ttc.png"; // Import the image file
+import "./App.css";
 
 function App() {
-
   const [routes, setRoutes] = useState([]);
 
   useEffect(() => {
     const getRoutes = async () => {
       const data = await get_routes();
       setRoutes(data);
-    }
+    };
 
     getRoutes();
-  }, [])
+  }, []);
 
   return (
     <>
-      {routes.length && <Menu route_info={routes}/>}
+      {/* Add the logo image */}
+      <img src={logo} alt="TTC Logo" className="logo" />
+
+      {/* Render the Menu component */}
+      {routes.length && <Menu route_info={routes} />}
     </>
-  )
+  );
 }
 
 export default App;
